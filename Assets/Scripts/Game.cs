6ee44f78eccf0edde30403941dfbd1eff7ef0845
Danilo Faria <danilofaria@outlook.com>
2015-05-10 03:31:24 -0400

    added text label

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Leap;
+
+public class Game : MonoBehaviour {
+
+	protected Controller leap_controller_;
+
+//	[SerializeField]
+	public Text gameTextDisplay;
+	
+	void Awake() {
+		leap_controller_ = new Controller();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (leap_controller_.Frame ().Hands.Count == 0) {
+			gameTextDisplay.text = "No hand";
+		} else
+		gameTextDisplay.text = "Hand";
+	}
+}
